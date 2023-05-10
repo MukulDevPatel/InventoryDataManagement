b514a52f106fb73dcbd6778b58a62ec91717b395
MukulDevPatel <patelmukuldev@gmail.com>
2023-05-10 21:50:12 +0530

    [Mukul Dev] Add: This is inventory data for rice, wheat and pulses

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace InventoryDataManagement
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Inventory Data Management");
+            string path = @"D:\BridgeLabz Second batch\DEMO-\InventoryDataManagement\InventoryData.json";
+            ReadTheData readTheData = new ReadTheData();
+            InventoryDetails data = readTheData.Read(path);
+            Console.WriteLine("Type Of Rices ");
+            Console.WriteLine("=================================================");
+            for (int i = 0; i < data.typeOfRice.Count; i++)
+            {
+                Console.WriteLine(data.typeOfRice[i].Name);
+                Console.WriteLine(data.typeOfRice[i].Weight);
+                Console.WriteLine(data.typeOfRice[i].Price);
+                int invebtoryprice = data.typeOfRice[i].Weight * data.typeOfRice[i].Price;
+
+                Console.WriteLine("The price of {0} for {1}kg is {2}Rs. ", data.typeOfRice[i].Name, data.typeOfRice[i].Weight, invebtoryprice);
+                Console.WriteLine("-------------------------------------------------");
+            }
+            Console.WriteLine("#####################################################");
+            Console.WriteLine("Type Of Wheats ");
+            Console.WriteLine("=================================================");
+            for (int i = 0; i < data.typeOfWheats.Count; i++)
+            {
+                Console.WriteLine(data.typeOfWheats[i].Name);
+                Console.WriteLine(data.typeOfWheats[i].Weight);
+                Console.WriteLine(data.typeOfWheats[i].Price);
+                int invebtoryprice = data.typeOfWheats[i].Weight * data.typeOfWheats[i].Price;
+
+                Console.WriteLine("The price of {0} for {1}kg is {2}Rs. ", data.typeOfWheats[i].Name, data.typeOfWheats[i].Weight, invebtoryprice);
+                Console.WriteLine("-------------------------------------------------");
+            }
+            Console.WriteLine("#####################################################");
+            Console.WriteLine("Type Of Pulses ");
+            Console.WriteLine("=================================================");
+            for (int i = 0; i < data.typeOfPulses.Count; i++)
+            {
+                Console.WriteLine(data.typeOfPulses[i].Name);
+                Console.WriteLine(data.typeOfPulses[i].Weight);
+                Console.WriteLine(data.typeOfPulses[i].Price);
+                int invebtoryprice = data.typeOfPulses[i].Weight * data.typeOfPulses[i].Price;
+                Console.WriteLine("The price of {0} for {1}kg is {2}Rs. ", data.typeOfPulses[i].Name, data.typeOfPulses[i].Weight, invebtoryprice);
+                Console.WriteLine("-------------------------------------------------");
+            }
+        }
+    }
+}
---
+++ ReadTheData.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace InventoryDataManagement
+{
+    public class ReadTheData
+    {
+       public InventoryDetails Read(string path)
+        {
+            using (StreamReader file = new StreamReader(path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<InventoryDetails>(json);
+                }catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    return null;
+                }
+            }
+        }
+    }
+}
