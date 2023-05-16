302972fb0b048fcf2bd29413b527fb50d2742892
MukulDevPatel <patelmukuldev@gmail.com>
2023-05-16 21:52:55 +0530

    [Mukul Dev] Add: Create stock account management program and calculate

---
+++ Stock.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPrograms
+{
+    public class Stock
+    {
+        public string Name { get; set; }
+        public int NumOfShares { get; set; }
+        public double SharePrice { get; set; }
+        public double Calculate()
+        {
+            return NumOfShares * SharePrice;
+        }
+    }
+}
